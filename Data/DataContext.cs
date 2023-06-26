08b6481782c8b1115410a2f21a13ce0c3c9d6706
Tiago Orlando <tiago_orlando@yahoo.com>
2023-06-26 01:28:00 -0300

    feat: added entity framework with bind for mysql

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using first_api.models;
+
+
+namespace first_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 global using first_api.service.character_service;
+global using Microsoft.EntityFrameworkCore;
+using first_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connStringName = "LocalConnection";
+var connString = builder.Configuration.GetConnectionString(connStringName);
 
+if (connString == null)
+{
+    throw new Exception($" connection string of name {connStringName} not found");
+}
+
+
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseMySql(connString, ServerVersion.AutoDetect(connString))
+);
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
