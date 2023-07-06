619014e7ebd1c5262e2380b4b1d7e596a7e772c8
Tiago Orlando <tiago_orlando@yahoo.com>
2023-07-06 17:37:09 -0300

    feat: added migration for inserting data in Skils Model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,6 +10,16 @@ namespace first_api.Data
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill {Id = 1, Name="Fireball", DamageVal = 34},
+                new Skill {Id = 2, Name="Lightning", DamageVal = 44},
+                new Skill {Id = 3, Name="Ice Shards", DamageVal = 34},
+                new Skill {Id = 4, Name="Poison Gas", DamageVal = 50}
+            );
+        }
+
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Weapon> Weapons => Set<Weapon>();
---
+++ Migrations/20230706165000_SkillSeeding.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace first_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SkillSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "DamageVal", "Name" },
+                values: new object[,]
+                {
+                    { 1, 34, "Fireball" },
+                    { 2, 44, "Lightning" },
+                    { 3, 34, "Ice Shards" },
+                    { 4, 50, "Poison Gas" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
